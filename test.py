5fe8af2ba56e25b51d5001ffdc0063161372c100
JKamlah <>
2020-01-15 18:33:06 +0100

    ADD argparse argument.

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
 import pytest
-from wordhunt import wordhunt
+import argparse
+from pathlib import Path
+from wordhunt import wordhunt, open_pdf
+
+arg_parser = argparse.ArgumentParser(description='Fuzzy word hunt in pdf files.')
+arg_parser.add_argument("pattern", type=str, help="file or folder path")
+arg_parser.add_argument("fpath", type=lambda x: Path(x), help="file or folder path", nargs='*')
+arg_parser.add_argument("-e", "--error", help="Maximum of possible character error", type=int, default=1)
+arg_parser.add_argument("-o", "--open", help="opens the first x pdfs in firefox (if installed)", type=int,default=0)
+arg_parser.add_argument("-v", "--verbose", help="shows information, like glyphe subsitutions", type=str, default="all",choices=["all","single","list"])
+args = arg_parser.parse_args()
 
 def test():
-	res = wordhunt("./Test/","Hey")
+	args.fpath = [Path("./Test/")]
+	args.pattern = "Hey"
+	res = wordhunt(args)
 	assert res != {},"test failed, empty result"
 	for reskey in res:
 		assert res[reskey][0]['Text'] == '>>Hey<<', "test failed, wrong result"
+
+def test2():
+	res = open_pdf("./Test/Test.pdf")
+	assert res.stderr.readline() == b'',"test failed, maybe firefox is not installed"
--- wordhunt.py
+++ wordhunt.py
@@ -1,35 +1,50 @@
+#!/usr/bin/env python3
 import pdfminer.high_level
+import subprocess
 from pathlib import Path
 from collections import defaultdict
-import sys
 import regex
+import argparse
 
-def wordhunt(fpath,pattern,error=1,verbose="all"):
-    fpath = Path(fpath)
+# Command line arguments.
+arg_parser = argparse.ArgumentParser(description='Fuzzy word hunt in pdf files.')
+arg_parser.add_argument("pattern", type=str, help="file or folder path")
+arg_parser.add_argument("fpath", type=lambda x: Path(x), help="file or folder path", nargs='*')
+arg_parser.add_argument("-e", "--error", help="Maximum of possible character error", type=int, default=1)
+arg_parser.add_argument("-o", "--open", help="opens the first x pdfs in firefox (if installed)", type=int,default=0)
+arg_parser.add_argument("-v", "--verbose", help="shows information, like glyphe subsitutions", type=str, default="all",choices=["all","single","list"])
+
+args = arg_parser.parse_args()
+
+def wordhunt(args):
+    if len(args.fpath) == 1 and not args.fpath[0].is_file():
+        args.fpath= list(Path(args.fpath[0]).rglob("*.pdf"))
     jsonres = defaultdict(list)
-    for fname in sorted(fpath.rglob("*.pdf")):
+    for fname in sorted(args.fpath):
         text = pdfminer.high_level.extract_text(fname)
         for idx, line in enumerate(text.splitlines()):
-
-            for res in regex.finditer(f"({pattern}){{e<={error}}}", line):
-                if verbose in ["all","single"]:
+            for res in regex.finditer(f"({args.pattern}){{e<={args.error}}}", line):
+                if args.verbose in ["all","single"]:
                     print(f"File:   {fname}\n"
-                        f"Line:   {idx}\n"
-                        f"Text:   {line[:res.span()[0]]+'>>'+line[res.span()[0]:res.span()[1]]+'<<'+line[res.span()[1]:]}\n"
-                        f"--------{'-'*len(line)}")
+                          f"Line:   {idx}\n"
+                          f"Text:   {line[:res.span()[0]]+'>>'+line[res.span()[0]:res.span()[1]]+'<<'+line[res.span()[1]:]}\n"
+                          f"------------{'-'*len(line)}")
                 jsonres[str(fname)].append({"Line":idx,"Text":line[:res.span()[0]]+'>>'+line[res.span()[0]:res.span()[1]]+'<<'+line[res.span()[1]:]})
-    if verbose in ["all","list"]:
+    if jsonres and args.verbose in ["all","list"] or args.open != 0:
         print(f"List of all PDFs with findings:")
-        for key in jsonres:
+        for idx, key in enumerate(jsonres):
             print(key)
+            if idx <= args.open-1:
+                open_pdf(key)
     return jsonres
 
+def open_pdf(fname):
+    pipe = subprocess.Popen([f'firefox "{fname}"'], shell=True,stderr=subprocess.PIPE)
+    return pipe
+
 if __name__ == "__main__":
-    fpath = sys.argv[1]
-    pattern = sys.argv[2]
-    error = sys.argv[3] if len(sys.argv) == 4 else int(len(pattern) / 10)
-    if error == 0: error = 1
-    wordhunt(fpath,pattern,error)
+    wordhunt(args)
+
 
 
 
